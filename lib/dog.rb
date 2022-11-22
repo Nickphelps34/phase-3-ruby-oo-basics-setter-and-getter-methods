0f574727a41b529bdd88ec185e2e03014aa4ca84
Nick Phelps <nickphelps34@gmail.com>
2022-11-22 12:02:29 -0500

    commit tree f66d568c60be3576653a503011fb9cbed0550533

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+  #Setter Method
+  def name ; (name)
+    @name = name
+  end
+
+  def breed ; (breed)
+    @breed = breed
+  end
+
+  #Setter Method
+  def name 
+    @name
+  end
+
+  def breed
+    @breed
+  end
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
-class Person
+require "pry"
 
+class Person
+  attr_accessor :name
+  #Setter Method
+  def initalization(name, job)
+    @name = name
+    @job = job
+  end
+
+  #Getter Method
+  # def name
+  #   @name
+  # end
+  
+  # def job
+  #   @job
+  # end
 end
+p1= Person.new('nick', "walker")
+
+binding.pry
+
